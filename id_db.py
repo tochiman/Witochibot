2fb77bf6ceff9bf75da960aa52b366f2aecc5e41
tochiman <yuutotochizawa@gmail.com>
2021-12-30 01:25:06 +0900

    update

--- id_db.py
+++ id_db.py
@@ -1,36 +1,46 @@
+from asyncio.windows_events import NULL
 import sqlite3
 import os
+import re
 
 os.chdir("C:/Users/yuuto/OneDrive/VS_code/python/Witochibot")
 
 dbname = 'id.db'
 conn = sqlite3.connect(dbname)
 
-#create table inquiry_id(id int primary key,guild_id int, user_id int);
-def inquiry_set(guild,user):
+#create table inquiry_id(id int primary key,guild_id int unique,user_id int unique);
+def inquiry_set(guild,user) -> None or False:
     cur = conn.cursor()
-    cur.execute('insert into inquiry_id(guild_id,user_id) values(?,?)',(guild,user,))
+    try:
+        cur.execute('insert into inquiry_id(guild_id,user_id) values(?,?)',(guild,user,))
+        conn.commit()
+        cur.close()
+        return None
+    except:
+        return False
+
+def inquiry_return(guild) -> int:
+    cur = conn.cursor()
+    cur.execute('select * from inquiry_id where guild_id=?',(guild,))
+    inq_return=str(cur.fetchall()).replace("[(None,","").replace(")]","")
+    inq_return=re.search(",.*",inq_return)
+    inq_return=inq_return.group()
+    inq_return=inq_return.replace(",","")
     conn.commit()
     cur.close()
+    return int(inq_return)
 
-def inquiry_return(guild):
+#create table channel_id(id int primary key,channel1,channel2); 
+def forwarding_channel_set(channel1,channel2)-> None:
     cur = conn.cursor()
-    print(cur.execute('select * from inquiry_id where guild_id=?',(guild)))
-    conn.commit()
-    cur.close()
-
-'''
-def forwarding_channel_set(channel1,channel2):
-    #create table channel_id(id int primary key,channel1,channel2); 
-    db_name = 'main.db'
-    db_connect = sqlite3.connect(db_name)
-    cur = sqlite3.Cursor(db_connect)
     cur.execute('insert into channel_id(channel1, channel2) values(?,?)',(channel1,channel2,))
+    conn.commit()
+    cur.close()
+    return None
 
-def forwarding_channel_return():
-    #create table channel_id(id int primary key,channel1,channel2); 
-    db_name = 'main.db'
-    db_connect = sqlite3.connect(db_name)
-    cur = sqlite3.Cursor(db_connect)
+def forwarding_channel_return()-> int:
+    cur = conn.cursor()
     cur.execute('select * from url')
-'''
+    conn.commit()
+    cur.close()
+    return int()
