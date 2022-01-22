0fce26c1e9942638a82c24b16ab2c57321f5d793
tochiman <yuutotochizawa@gmail.com>
2022-01-23 01:20:49 +0900

    tweet_set_channel

--- id_db.py
+++ id_db.py
@@ -51,6 +51,17 @@ def inquiry_update(guild,user)-> None or False:
         cur.close()
         return False
 
+# create table tweet_channel(guild_id int unique, channel_id int);
+def tweet_set_channel(guild,channel) -> None or False:
+    cur = conn.cursor()
+    try:
+        cur.execute('insert into tweet_channel(guild_id,channel_id) values(?,?)',(guild,channel,))
+        conn.commit()
+        cur.close()
+        return None
+    except:
+        return False
+
 '''
 #create table inquiry_num(guild_id int,user_id int,number int unique, inquiry_content string);
 def inquiry_num_set(guild,user,content) -> str:
@@ -73,11 +84,11 @@ def inquiry_num_return(guild,user,number):
     cur.close()
 '''
 
-#create table channel_id(id int primary key, guild_id int ,channel1 int unique,channel2 int unique);
-def forwarding_channel_set(guild,channel1,channel2)-> None or False:
+#create table channel_id(guild_id1 int unique, guild_id2 int unique,channel1 int unique, channel2 int unique );
+def forwarding_channel_set(guild1,guild2,channel1,channel2)-> None or False:
     cur = conn.cursor()
     try:
-        cur.execute('insert into channel_id(guild_id,channel1, channel2) values(?,?,?)',(guild,channel1,channel2,))
+        cur.execute('insert into channel_id(guild_id1,guild_id2,channel1, channel2) values(?,?,?,?)',(guild1,guild2,channel1,channel2,))
         conn.commit()
         cur.close()
         return None
@@ -87,12 +98,14 @@ def forwarding_channel_set(guild,channel1,channel2)-> None or False:
 
 def forwarding_channel_return(guild)-> int:
     cur = conn.cursor()
-    cur.execute('select * from channel_id where guild_id=?',(guild,))
-    channel_return_id = cur.fetchall()
-    print(channel_return_id)
+    cur.execute('select * from channel_id where guild_id1=? or guild_id2=?',(guild,guild))
+    channel_return_id = str(cur.fetchall()).replace(f"({guild}","")
+    channel_return_1 = re.match("\[,.+,",channel_return_id)
+    channel_return_1 = channel_return_1.group().replace("[","").replace(",","")
+    channel_return_2 = channel_return_id.replace("[,","").replace(f"{channel_return_1},","").replace(")]","")
     conn.commit()
     cur.close()
-    return int(channel_return_id)
+    return int(channel_return_1),int(channel_return_2)
 
 def forwarding_channel_del(channel1,channel2) -> None:
     try:
@@ -104,3 +117,4 @@ def forwarding_channel_del(channel1,channel2) -> None:
     except:
         cur.close()
         return False
+
